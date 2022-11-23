9cd5e6a7d7572b3769ba4ea104efdefcefda29d2
bennizri <bennizri22@gmail.com>
2022-11-23 14:07:35 +0200

    app

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,9 +4,11 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
+      <ul>
+        <div className="expense-item__description "> {props.title}</div>
+        <div className="expense-item__price"> {props.amount}</div>
+      </ul>
       <ExpenseImg Image={props.Image} />
-      <div className="expense-item__description "> {props.title}</div>
-      <div className="expense-item__price"> {props.amount}</div>
     </div>
   );
 }
